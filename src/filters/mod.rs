a83ad2fe64f8257144aa3969ed30902b6b428201
Andrew Banchich <andrewbanchich@gmail.com>
2019-04-26 23:30:05 -0400

    Import all filter modules

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -1,14 +1,3 @@
-mod grok;
-pub use grok::*;
-mod date;
-pub use date::*;
-mod geoip;
-pub use geoip::*;
-mod mutate;
-pub use mutate::*;
-mod clone;
-pub use clone::*;
-
 use futures::{
     sync::mpsc::{channel, Receiver, Sender},
     Future, Poll, Sink, Stream,
@@ -135,3 +124,92 @@ impl Future for Filter {
         }
     }
 }
+
+mod aggregate;
+pub use aggregate::*;
+mod alter;
+pub use alter::*;
+mod bytes;
+pub use bytes::*;
+mod cidr;
+pub use cidr::*;
+mod cipher;
+pub use cipher::*;
+mod clone;
+pub use clone::*;
+mod csv;
+pub use csv::*;
+mod date;
+pub use date::*;
+mod de_dot;
+pub use de_dot::*;
+mod dissect;
+pub use dissect::*;
+mod dns;
+pub use dns::*;
+mod drop;
+pub use drop::*;
+mod elapsed;
+pub use elapsed::*;
+mod elasticsearch;
+pub use elasticsearch::*;
+mod environment;
+pub use environment::*;
+mod extractnumbers;
+pub use extractnumbers::*;
+mod fingerprint;
+pub use fingerprint::*;
+mod geoip;
+pub use geoip::*;
+mod grok;
+pub use grok::*;
+mod http;
+pub use http::*;
+mod i18n;
+pub use i18n::*;
+mod jdbc_static;
+pub use jdbc_static::*;
+mod jdbc_streaming;
+pub use jdbc_streaming::*;
+mod json;
+pub use json::*;
+mod json_encode;
+pub use json_encode::*;
+mod kv;
+pub use kv::*;
+mod memcached;
+pub use memcached::*;
+mod metricize;
+pub use metricize::*;
+mod metrics;
+pub use metrics::*;
+mod mutate;
+pub use mutate::*;
+mod prune;
+pub use prune::*;
+mod range;
+pub use range::*;
+mod ruby;
+pub use ruby::*;
+mod sleep;
+pub use sleep::*;
+mod split;
+pub use split::*;
+mod syslog_pri;
+pub use syslog_pri::*;
+mod throttle;
+pub use throttle::*;
+mod tld;
+pub use tld::*;
+mod translate;
+pub use translate::*;
+mod truncate;
+pub use truncate::*;
+mod urldecode;
+pub use urldecode::*;
+mod useragent;
+pub use useragent::*;
+mod uuid;
+pub use uuid::*;
+mod xml;
+pub use xml::*;
