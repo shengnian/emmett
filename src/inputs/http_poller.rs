a6c7c7f62207440ea28a05463205b96a4f7fc691
Andrew Banchich <andrewbanchich@gmail.com>
2019-05-04 23:37:06 -0400

    Implement more HttpPollerInput settings

--- src/inputs/http_poller.rs
+++ src/inputs/http_poller.rs
@@ -1,10 +1,10 @@
-#![allow(unused)]
-
 /// Specification: https://www.elastic.co/guide/en/logstash/current/plugins-inputs-http_poller.html
 use futures::{stream::iter_ok, sync::mpsc::Sender, try_ready, Async, Poll, Stream};
-use reqwest::{ClientBuilder, RedirectPolicy};
+use reqwest::{Client, RedirectPolicy, Proxy, Certificate};
 use serde_json::value::Value;
 use std::path::Path;
+use std::fs::File;
+use std::io::Read;
 use std::time::Duration;
 use tokio::timer::Interval;
 
@@ -13,34 +13,79 @@ impl<'a> Stream for HttpPoller<'a> {
     type Error = ();
 
     fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        let mut client = ClientBuilder::new();
 
-        // self.follow_redirects
+        let mut client = Client::builder();
+
+        // automatic_retries
+        // not implemented in Reqwest yet
+
+        // cacert
+        if let Some(cacert_path) = self.cacert {
+
+            let mut buf = Vec::new();
+            File::open(cacert_path)
+                .expect("Couldn't find CA file.")
+                .read_to_end(&mut buf)
+                .expect("Couldn't read CA file.");
+
+            let cert = Certificate::from_der(&buf)
+                .expect("Certificate cannot be created from file.");
+            
+            client = client.add_root_certificate(cert);
+        }
+
+        // client_cert
+
+        // client_key
+
+        // connect_timeout
+        if let Some(timeout) = self.connect_timeout {
+            client = client.timeout(timeout);
+        }
+
+        // cookies
+        client = client.cookie_store(self.cookies);
+
+        // follow_redirects
         if self.follow_redirects == Some(true) {
             client = client.redirect(RedirectPolicy::default());
         }
 
-        // self.request_timeout
+        // keepalive
+
+        // keystore and keystore_password
+
+        // keystore_type
+
+        // pool_max
+        client = client.max_idle_per_host(self.pool_max);
+
+        // pool_max_per_route
+        // careful about this and pool_max
+
+        // proxy
+        if let Some(proxy) = self.proxy.to_owned() {
+            client = client.proxy(proxy);
+        }
+        
+        // request_timeout
         if let Some(timeout) = self.request_timeout {
             client = client.timeout(Duration::from_secs(timeout));
         }
 
-        // self.cookies
-        if self.cookies == Some(true) {
-            client = client.cookie_store(true);
-        }
+        // retry_non_idempotent
 
         let client = client.build().expect("Couldn't build Reqwest client.");
 
-        // self.schedule
+        // schedule
         try_ready!(self.schedule.poll().map_err(|_| ()));
 
-        // self.urls
+        // urls
         let mut response_stream = iter_ok(self.urls.to_owned()).and_then(|uri| {
             let url = url::Url::parse(uri).unwrap();
             let mut req = client.request(http::Method::GET, url);
 
-            // self.user and self.password
+            // user and password
             if let Some(user) = self.user {
                 req = req.basic_auth::<&str, &str>(user, self.password);
             }
@@ -51,6 +96,9 @@ impl<'a> Stream for HttpPoller<'a> {
                 .json()
                 .expect("Couldn't parse HttpPoller response as JSON.");
 
+            // metadata_target
+
+            
             Ok(res)
         });
 
@@ -68,17 +116,17 @@ pub struct HttpPoller<'a> {
     cacert: Option<&'a Path>,
     client_cert: Option<&'a Path>,
     client_key: Option<&'a Path>,
-    connect_timeout: Option<u64>,
-    cookies: Option<bool>,
+    connect_timeout: Option<Duration>,
+    cookies: bool,
     follow_redirects: Option<bool>,
     keepalive: Option<bool>,
     keystore: Option<&'a Path>,
     keystore_password: Option<&'a str>,
     keystore_type: Option<&'a str>,
-    metadata_target: Option<&'a str>,
-    pool_max: Option<u64>,
+    metadata_target: &'a str,
+    pool_max: usize,
     pool_max_per_route: Option<u64>,
-    proxy: Option<&'a str>,
+    proxy: Option<Proxy>,
     request_timeout: Option<u64>,
     retry_non_idempotent: Option<bool>,
     schedule: Interval,
@@ -101,15 +149,15 @@ impl<'a> Default for HttpPoller<'a> {
             cacert: None,
             client_cert: None,
             client_key: None,
-            connect_timeout: Some(10),
-            cookies: Some(true),
+            connect_timeout: Some(Duration::from_secs(10)),
+            cookies: true,
             follow_redirects: Some(true),
             keepalive: Some(true),
             keystore: None,
             keystore_password: None,
             keystore_type: Some("JKS"),
-            metadata_target: Some("@metadata"),
-            pool_max: Some(50),
+            metadata_target: "@metadata",
+            pool_max: 50,
             pool_max_per_route: Some(25),
             proxy: None,
             request_timeout: Some(60),
