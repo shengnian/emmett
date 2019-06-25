9d6f07f1ec64489aa5814e3a97b041c2f3aa1862
Andrew Banchich <andrewbanchich@gmail.com>
2019-06-24 22:39:02 -0400

    Restructure to bin / lib hybrid

---
+++ src/bin.rs
@@ -0,0 +1,13 @@
+use futures::future::lazy;
+use std::path::Path;
+
+use emmett::Pipeline;
+
+fn main() {
+    let example_config = Path::new("./example_configs/full.toml");
+    
+    tokio::run(lazy(move || {
+        Pipeline::from_toml(example_config).run();
+        Ok(())
+    }));
+}
